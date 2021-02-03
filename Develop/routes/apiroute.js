7d3e48949899350f9dbb977d23b36f8913954ddf
Young Ji Kim <youjmik@gmail.com>
2021-02-03 11:31:20 -0500

    Working on API Route GET

--- Develop/routes/apiroute.js
+++ Develop/routes/apiroute.js
@@ -1,18 +1,40 @@
 const dbjson = require("../db/db.json")
+const fs = require("fs")
+const path = require("path")
+
 
 
 
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
-        res.json(dbjson)
-    });
-    app.post('/api/notes', (req, res) => {
-        dbjson.push(req.body)
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+            if (err) throw (err)
+            else (console.log(data))
+            data = JSON.parse(data)
+            console.log(data)
+            res.json(data)
+        })
+        
+
     });
 
+
+    app.post('/api/notes', (req, res) => {
+        dbjson.push(req.body)
+
+
+        //how to get post route to render a page//
+
+        //data is saving but not rendering//
+    });
+    // work on this with res. 
 }
 
 // app.delete("/api/notes/:id", (req, res) => {
-//     //// FIGURE OUT HOW TO DO BONUS
+//     const deleteApp = req.params.id
+//     console.log (deleteApp)
+
 // })
+
+//     //// FIGURE OUT HOW TO DO BONUS
